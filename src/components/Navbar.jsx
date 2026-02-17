22a2c989530baf034be87d145e192a102a9c86b4
Anna Eliza <annaeliza.als@gmail.com>
2026-02-17 20:45:25 -0300

    correção
    correção do hover do navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,13 +2,15 @@ import { NavLink } from "react-router-dom";
 import ThemeToggle from "./ThemeToggle";
 
 export default function Navbar() {
-
   return (
     <nav className="navbar">
-      <div className="container">
-        <NavLink to="/">{"Início"}</NavLink>
-        <NavLink to="/projetos">{"Projetos"}</NavLink>
-        <NavLink to="/sobre">{"Sobre"}</NavLink>
+      <div className="container navbar-content">
+        <div className="nav-links">
+          <NavLink to="/" end>Início</NavLink>
+          <NavLink to="/projetos">Projetos</NavLink>
+          <NavLink to="/sobre">Sobre</NavLink>
+        </div>
+
         <ThemeToggle />
       </div>
     </nav>
