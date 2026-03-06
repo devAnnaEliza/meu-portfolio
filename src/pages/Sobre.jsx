fc40b1a3b6e015e389cd19307782b7c1aa5f530b
Anna Eliza <annaeliza.als@gmail.com>
2026-03-06 18:24:45 -0300

    correção sobre

--- src/pages/Sobre.jsx
+++ src/pages/Sobre.jsx
@@ -1,7 +1,7 @@
 export default function Sobre() {
   return (
     <section className="sobre">
-      <div className="sobre-container">
+      <div className="container">
 
         <h2 className="sobre-titulo">Sobre</h2>
 
